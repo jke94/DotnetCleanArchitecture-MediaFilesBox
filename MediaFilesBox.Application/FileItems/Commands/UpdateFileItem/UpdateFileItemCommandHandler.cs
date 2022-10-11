8463699f26e82e372479ba9f73f4fe6e854f42ff
jke94 <jcarracedo.dev@gmail.com>
2022-10-11 11:57:09 +0200

    Rename namespace to FileItem.

---
+++ MediaFilesBox.Application/FileItems/Commands/UpdateFileItem/UpdateFileItemCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaFilesBox.Application.FileItems.Commands.UpdateMediaFile
+{
+    internal class UpdateFileItemCommandHandler
+    {
+    }
+}
